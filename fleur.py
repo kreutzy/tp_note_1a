7c2be57892eeb33d4f03ebf9097192100fc45fb7
Mathieu <mathieu.thomassin@eleve.ensai.fr>
2021-03-22 20:55:54 +0100

    premier commit

---
+++ fleur.py
@@ -0,0 +1,34 @@
+# 2. [Classe Fleur] 
+# Implémentez la classe Fleur, sans oublier la liste des pétales indiquée par l'association 
+# entre Fleur et Petale de la Figure 1. Le statut de la fleur dépend de ses attributs:
+# âge jusqu'à 15 jours : en fleur
+# âge entre 16 et 20 jours : fanant
+# âge entre 21 et 30 jours, et fécondée : fruit
+# âge plus de 21 jours non fécondée, ou plus de 31 jours ; tombée
+# a) [Attributs]    Les fleurs ont un âge et un statut fécondée ou non
+
+class Fleur:
+    ''' Fleur
+    
+    Attributes
+    ----------
+    age: int
+        âge de la fleur
+    fecondee: bool
+        est vraie si la fleur est fécondée, faux sinon
+        
+    Parameters
+    ----------
+    
+    '''
+    def __init__(self):
+        pass
+    def est_fanant():
+        pass
+    def est_fruit():
+        pass
+    def vieillir():
+        pass
+    def couleur():
+        pass
+    
---
+++ petale.py
@@ -0,0 +1,72 @@
+class Petale():
+    ''' Petale
+    
+    Attributes
+    ----------
+    age: int
+        âge du pétale
+        
+    Parameters
+    ----------
+    
+    '''
+    def __init__(self):
+        '''Création d'un pétale
+        
+        Parameters
+        ----------
+        
+        '''
+        self.age=0
+        
+    def vieillir():
+        ''' Incrémente l'âge et teste si le pétale
+        est toujours accroché
+        
+        Parameters
+        ----------
+        
+        Returns
+        -------
+        bool
+            True si le pétale est toujours accroché
+            
+        Examples
+        --------
+        >>>monpetale.vieillir()
+        TRUE
+        '''
+        estaccroche = True
+        self.age += 1
+        
+        if self.age <20:
+            estaccroche = False
+        return estaccroche
+        
+    def couleur():
+         ''' Retourne 'rouge' pour un pétale non fané
+         'marron' pour un pétale commençant à faner
+        
+        Parameters
+        ----------
+        
+        Returns
+        -------
+        str
+            'rouge' ou 'marron'
+            
+        Examples
+        --------
+        >>>monpetale = Petale()
+        >>>monpetale.couleur()
+        'rouge'
+        '''
+        couleur_non_fane = 'rouge'
+        couleur_fane = 'marron'
+        if self.age<15:
+            return couleur_non_fane
+        else:
+            return couleur_fane
+        
+        
+        
